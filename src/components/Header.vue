8b4496e7dc8d6d88f71581c78ae7319b7856fe8a
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-06-24 15:57:20 -0300

    getting User from input

---
+++ src/components/Header.vue
@@ -0,0 +1,74 @@
+<template>
+  <header id="header">
+    <h2><strong>Github</strong> Search</h2>
+    <form>
+      <input
+        type="text"
+        name="profile"
+        v-model="profile"
+        placeholder="Write your github profile"
+      />
+      <button @click="getUserProfile">enviar</button>
+    </form>
+  </header>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Header',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      profile: document.querySelector('imput[name=profile]'),
+    };
+  },
+  methods: {
+    getUserProfile(e) {
+      e.preventDefault();
+      axios
+        .get(`https://api.github.com/users/${this.profile}/repos`)
+        .then((data) => {
+          console.log(data);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+#header {
+  width: 80%;
+  max-width: 1360px;
+  display: flex;
+  justify-content: space-between;
+  margin: 40px auto;
+  font-family: 'Rubik';
+}
+#header h2 {
+  font-style: italic;
+  font-weight: 300;
+  font-size: 36px;
+}
+#header strong {
+  font-style: initial;
+  font-weight: 700;
+}
+#header input {
+  width: 400px;
+  height: 40px;
+  border: 2px solid #000000;
+  padding-left: 10px;
+  outline: none;
+}
+#header button {
+  width: 60px;
+  height: 40px;
+  background-color: #000000;
+  border: none;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/Repositories.vue
@@ -0,0 +1,37 @@
+
+
+<template>
+  <div class="repository">
+    <h1 class="title">sistema-comentario</h1>
+    <p class="description">Descriçao: Experimentações Sistema de comentários usando vue-js</p>
+    <span><img src="" alt=""> 0</span>
+  </div>
+</template>
+
+<script> 
+export default {
+  name: 'Repositories',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .repository{
+  font-family: 'Rubik', sans-serif;
+  font-weight:300
+ }   
+  h1 {
+    font-size:24px;
+  }
+  p {
+    font-size:16px;
+    counter-reset: #757575;
+    line-height: 28px;
+  }
+  span {
+    margin-bottom: 25px;
+  }
+</style>
